ecb2c5eec08853c411b67e86402c06e37b795742
Restyled.io <commits@restyled.io>
2023-04-19 22:27:59 +0000

    Restyled by yapf

--- servico_de_mensagem.py
+++ servico_de_mensagem.py
@@ -13,20 +13,19 @@ def obter_dados_da_mensagem(request):
     json_convertido = converter_request_para_objeto(request)
 
     valor = json_convertido[constantes_do_servico_de_mensagem.entry][
-        constantes_do_servico_de_mensagem.zero
-    ][constantes_do_servico_de_mensagem.changes][
-        constantes_do_servico_de_mensagem.zero
-    ][
-        constantes_do_servico_de_mensagem.value
-    ]
+        constantes_do_servico_de_mensagem.zero][
+            constantes_do_servico_de_mensagem.changes][
+                constantes_do_servico_de_mensagem.zero][
+                    constantes_do_servico_de_mensagem.value]
     tipo = valor[constantes_do_servico_de_mensagem.messages][
-        constantes_do_servico_de_mensagem.zero
-    ][constantes_do_servico_de_mensagem.type]
+        constantes_do_servico_de_mensagem.zero][
+            constantes_do_servico_de_mensagem.type]
     message = valor[constantes_do_servico_de_mensagem.messages][
-        constantes_do_servico_de_mensagem.zero
-    ][constantes_do_servico_de_mensagem.text][constantes_do_servico_de_mensagem.body]
+        constantes_do_servico_de_mensagem.zero][
+            constantes_do_servico_de_mensagem.text][
+                constantes_do_servico_de_mensagem.body]
     contact = valor[constantes_do_servico_de_mensagem.contacts][
-        constantes_do_servico_de_mensagem.zero
-    ][constantes_do_servico_de_mensagem.wa_id]
+        constantes_do_servico_de_mensagem.zero][
+            constantes_do_servico_de_mensagem.wa_id]
 
     return {"tipo": tipo, "message": message, "contato": contact}
